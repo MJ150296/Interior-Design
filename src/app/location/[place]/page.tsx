3264bcdc9fbb69d0a74d4ba4071f5508b29e1290
MJ150296 <joshi.mayank5286@gmail.com>
2025-04-12 10:37:45 +0530

    removed us setae and use effect

--- src/app/location/[place]/page.tsx
+++ src/app/location/[place]/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useParams } from "next/navigation";
 import Image from "next/image";
 import { FaTools, FaSyncAlt, FaTint, FaHandHolding } from "react-icons/fa";
