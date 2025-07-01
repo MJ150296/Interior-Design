e2c101b0b240c1aa19c5f60d1928e636b7616b45
MJ150296 <joshi.mayank5286@gmail.com>
2025-07-02 03:24:39 +0530

    removed chevron down

--- src/app/components/Header/Navbar.tsx
+++ src/app/components/Header/Navbar.tsx
@@ -2,7 +2,7 @@
 
 import React, { useEffect } from "react";
 import Link from "next/link";
-import { Menu, ChevronDown } from "lucide-react";
+import { Menu } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import {
   Sheet,
